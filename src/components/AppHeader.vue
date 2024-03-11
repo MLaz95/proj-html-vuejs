eb6d2de3e28ed24a9b98279df4811a73131e477a
marco <lazzari.marco.ml@gmail.com>
2024-03-11 15:27:17 +0100

    layout base header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,8 +11,48 @@
 </script>
 
 <template>
-    <NavBar></NavBar>
+    <header>
+        <NavBar></NavBar>
+        <img src="/img/h5-slide-3-background.jpg" alt="">
+        <div class="head-text">
+            <h1>Contemporary Ideas</h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nobis molestiae architecto soluta provident esse sit vel assumenda in natus!
+            </p>
+            <button>Register Now</button>
+        </div>
+    </header>
+
 
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+    header{
+        position: relative;
+        img{
+            width: 100%;
+        }
+        
+        .head-text{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+
+            text-align: center;
+            color: white;
+
+            h1{
+                font-size: 4rem;
+                text-wrap: nowrap;
+            }
+
+            p{
+                font-size: 1rem;
+                line-height: 1.5rem;
+                margin: 1rem 0;
+            }
+        }
+    }
+
+</style>
